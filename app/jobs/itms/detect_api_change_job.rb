c3db50020b194eafa4515bdc29377c478befad54
Ernest Walzel <ewalzel@gmail.com>
2017-10-05 22:57:55 +0200

    API change detection job

---
+++ app/jobs/itms/detect_api_change_job.rb
@@ -0,0 +1,16 @@
+require 'harvester_utils/downloader'
+
+class Itms::DetectApiChangeJob < ApplicationJob
+  LAST_KNOWN_SWAGGER_PATH = Pathname.new('vendor/itms/swagger.json')
+  ApiChangeDetectedException = Class.new(RuntimeError)
+
+  def perform(downloader: HarvesterUtils::Downloader)
+    current_swagger = downloader.get('https://opendata.itms2014.sk/v2/swagger.json').body
+    last_known_swagger = File.read(LAST_KNOWN_SWAGGER_PATH)
+
+    current_swagger_json = JSON.parse(current_swagger)
+    last_known_swagger_json = JSON.parse(last_known_swagger)
+
+    raise ApiChangeDetectedException unless current_swagger_json == last_known_swagger_json
+  end
+end
