5599b7a6d6bba7976f4cc529be20d06252eb728e
Ernest Walzel <ewalzel@gmail.com>
2017-10-06 11:06:16 +0200

    Add codelist attributes to Itms::Discrepancy

--- app/models/itms/discrepancy.rb
+++ app/models/itms/discrepancy.rb
@@ -1,3 +1,7 @@
 class Itms::Discrepancy < ApplicationRecord
   self.table_name = 'itms.nezrovnalosti'
+
+  belongs_to :administrativny_stav, class_name: Itms::Code
+  belongs_to :financny_stav, class_name: Itms::Code
+  belongs_to :hlavny_typ_nezrovnalosti, class_name: Itms::Code
 end
