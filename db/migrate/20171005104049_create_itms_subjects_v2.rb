ce744b002aa0fd81722f0cf428f90e98a8a4e083
Ernest Walzel <ewalzel@gmail.com>
2017-11-15 17:23:04 +0100

    Update Itms::Subject (subjekty) and its sync job

--- db/migrate/20171005104049_create_itms_subjects_v2.rb
+++ db/migrate/20171005104049_create_itms_subjects_v2.rb
@@ -1,23 +1,21 @@
-class CreateSubjects < ActiveRecord::Migration[5.0]
+class CreateItmsSubjectsV2 < ActiveRecord::Migration[5.0]
   def change
     drop_table 'itms.subjekty'
     
     create_table 'itms.subjekty' do |t|
       t.integer :itms_id, null: false, index: { unique: true}
       t.string :itms_href
-      t.datetime :itms_created_at
-      t.datetime :itms_updated_at
+      t.string :itms_created_at
+      t.string :itms_updated_at
 
       t.string :dic
-      t.decimal :gps_lat
-      t.decimal :gps_lon
       t.string :ico
       t.string :ine_identifikacne_cislo
       t.string :nazov
-      t.string :obec
+      t.references :obec, foreign_key: { to_table: 'itms.nuts_kody' }
       t.string :psc
-      t.string :stat
-      t.string :typ_ineho_identifikacneho_cisla
+      t.references :stat, foreign_key: { to_table: 'itms.hodnoty_ciselnikov' }
+      t.references :typ_ineho_identifikacneho_cisla, foreign_key: { to_table: 'itms.hodnoty_ciselnikov' }
       t.string :ulica
       t.string :ulica_cislo
 
