7875d8d60038421dff3f9a42990ae374e8582a1e
Ernest Walzel <ewalzel@gmail.com>
2017-11-21 19:39:55 +0100

    Update Itms::SyncActivityTypeJob to take href instead of ID

--- app/jobs/itms/sync_activity_type_job.rb
+++ app/jobs/itms/sync_activity_type_job.rb
@@ -1,12 +1,13 @@
 require 'harvester_utils/downloader'
 
 class Itms::SyncActivityTypeJob < ItmsJob
-  def perform(itms_id, downloader: HarvesterUtils::Downloader)
-    response = downloader.get("https://opendata.itms2014.sk/v2/typyAktivit/#{itms_id}")
+  def perform(itms_href, downloader: HarvesterUtils::Downloader)
+    response = downloader.get("https://opendata.itms2014.sk#{itms_href}")
     json = JSON.parse(response.body)
+    itms_id = itms_href.split('/').last
 
     ActiveRecord::Base.transaction do
-      u = Itms::ActivityType.find_or_initialize_by(itms_id: itms_id)
+      u = Itms::ActivityType.find_or_create_by!(itms_id: itms_id)
       u.itms_href = json['href']
       u.itms_created_at = json['createdAt']
       u.itms_updated_at = json['updatedAt']
