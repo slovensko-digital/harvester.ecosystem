17bbc071e80a199598f773154315655c966b4bb5
Ernest Walzel <ewalzel@gmail.com>
2017-11-21 17:31:40 +0100

    Itms::SyncAllProjectIndicators Change has_many :through definitions to has_an_belongs to many. Remove unnecessary model files

--- app/models/itms/project_indicator_completion_time.rb
+++
@@ -1,6 +0,0 @@
-class Itms::ProjectIndicatorCompletionTime < ApplicationRecord
-  self.table_name = 'itms.projektove_ukazovatele_casy_plnenia'
-
-  belongs_to :projektovy_ukazovatel, class_name: Itms::ProjectIndicator
-  belongs_to :kod, class_name: Itms::CodelistValue
-end
