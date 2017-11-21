1317e79ab294434bca6d9cebf12c3fe6a3058835
Ernest Walzel <ewalzel@gmail.com>
2017-11-21 20:25:20 +0100

    Update Itms::SyncAllSpecificGoalsJob to construct its own list of objects to sync from API

--- app/jobs/itms/sync_all_specific_goals_job.rb
+++ app/jobs/itms/sync_all_specific_goals_job.rb
@@ -1,9 +1,24 @@
 require 'harvester_utils/downloader'
 
 class Itms::SyncAllSpecificGoalsJob < ItmsJob
-  def perform
-    Itms::SpecificGoal.find_each do |specific_goal|
-      Itms::SyncSpecificGoalJob.perform_later(specific_goal.itms_href)
+  def perform(downloader: HarvesterUtils::Downloader)
+    response = downloader.get('https://opendata.itms2014.sk/v2/operacneProgramy')
+    operational_programs_ids = JSON.parse(response.body).map { |json| json['id'] }
+
+    priority_axes_ids = []
+    operational_programs_ids.map do |op_id|
+      response = downloader.get("https://opendata.itms2014.sk/v2/operacneProgramy/#{op_id}/prioritneOsi")
+      priority_axes_ids.concat(JSON.parse(response.body).map { |json| json['id'] })
+    end
+
+    specific_goals_hrefs = []
+    priority_axes_ids.map do |pa_id|
+      response = downloader.get("https://opendata.itms2014.sk/v2/prioritnaOs/#{pa_id}/konkretneCiele")
+      specific_goals_hrefs.concat(JSON.parse(response.body).map { |json| json['href'] })
+    end
+
+    specific_goals_hrefs.map do |specific_goals_href|
+      Itms::SyncSpecificGoalJob.perform_later(specific_goals_href)
     end
   end
 end
--- spec/jobs/itms/sync_all_specific_goals_job_spec.rb
+++ spec/jobs/itms/sync_all_specific_goals_job_spec.rb
@@ -1,20 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Itms::SyncAllSpecificGoalsJob, type: :job do
+  let(:downloader) { double(:downloader) }
+
   context '#perform' do
     ActiveJob::Base.queue_adapter = :test
 
     it 'syncs all specific goals' do
-      Itms::SpecificGoal.create!(itms_id: 1, itms_href: '/v2/konkretnyCiel/1')
-      Itms::SpecificGoal.create!(itms_id: 2, itms_href: '/v2/konkretnyCiel/2')
-      Itms::SpecificGoal.create!(itms_id: 3, itms_href: '/v2/konkretnyCiel/3')
+      expect(downloader)
+          .to receive(:get)
+          .with('https://opendata.itms2014.sk/v2/operacneProgramy')
+          .and_return(double(body: itms_file_fixture('operacny_program_list.json')))
+          .once
 
-      subject.perform
+      expect(downloader)
+          .to receive(:get)
+          .with(/\/v2\/operacneProgramy\/\d+\/prioritneOsi$/)
+          .and_return(double(body: itms_file_fixture('prioritna_os_list.json')))
+          .exactly(10).times
 
-      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.exactly(3).times
-      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.with('/v2/konkretnyCiel/1')
-      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.with('/v2/konkretnyCiel/2')
-      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.with('/v2/konkretnyCiel/3')
+      expect(downloader)
+          .to receive(:get)
+          .with(/\/v2\/prioritnaOs\/\d+\/konkretneCiele$/)
+          .and_return(double(body: itms_file_fixture('konkretny_ciel_list.json')))
+          .exactly(10 * 5).times
+
+      subject.perform(downloader: downloader)
+
+      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.exactly(10 * 5 * 7).times
+      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.with('/v2/konkretnyCiel/1').at_least(:once)
+      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.with('/v2/konkretnyCiel/2').at_least(:once)
+      expect(Itms::SyncSpecificGoalJob).to have_been_enqueued.with('/v2/konkretnyCiel/3').at_least(:once)
     end
   end
 end
