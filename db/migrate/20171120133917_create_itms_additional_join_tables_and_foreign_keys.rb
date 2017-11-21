2e344d3438b6c2f2d9f8f907ffb7047b0cf26320
Ernest Walzel <ewalzel@gmail.com>
2017-11-21 13:37:59 +0100

    Add missing attributes to Itms::SyncAccountsReceivableDocumentJob

--- db/migrate/20171120133917_create_itms_additional_join_tables_and_foreign_keys.rb
+++ db/migrate/20171120133917_create_itms_additional_join_tables_and_foreign_keys.rb
@@ -21,5 +21,28 @@ class CreateItmsAdditionalJoinTablesAndForeignKeys < ActiveRecord::Migration[5.0
               [:nezrovnalost_id, :zop_id],
               name: 'index_itms.nezrovnalosti_suvisiace_zop_on_nezrovnalost_and_zop',
               unique: true
+
+
+    add_foreign_key 'itms.pohladavkove_doklady', 'itms.projekty', column: :projekt_id
+
+    create_table 'itms.pohladavkove_doklady_suvisiace_verejne_obstaravania' do |t|
+      t.references :pohladavkovy_doklad, null: false, foreign_key: { to_table: 'itms.pohladavkove_doklady' }, index: { name: 'index_itms.pohladavkove_doklady_suvisiace_vo_on_pd' }
+      t.references :verejne_obstaravanie, null: false, foreign_key: { to_table: 'itms.verejne_obstaravania' }, index: { name: 'index_itms.pohladavkove_doklady_suvisiace_vo_on_vo' }
+      t.timestamps
+    end
+    add_index 'itms.pohladavkove_doklady_suvisiace_verejne_obstaravania',
+              [:pohladavkovy_doklad_id, :verejne_obstaravanie_id],
+              name: 'index_itms.pohladavkove_doklady_suvisiace_vo_on_pd_and_vo',
+              unique: true
+
+    create_table 'itms.pohladavkove_doklady_suvisiace_zop' do |t|
+      t.references :pohladavkovy_doklad, null: false, foreign_key: { to_table: 'itms.pohladavkove_doklady' }, index: { name: 'index_itms.pohladavkove_doklady_suvisiace_zop_on_pd' }
+      t.references :zop, null: false, foreign_key: { to_table: 'itms.zop' }, index: true
+      t.timestamps
+    end
+    add_index 'itms.pohladavkove_doklady_suvisiace_zop',
+              [:pohladavkovy_doklad_id, :zop_id],
+              name: 'index_itms.pohladavkove_doklady_suvisiace_zop_on_pd_and_zop',
+              unique: true
   end
 end
--- spec/jobs/itms/sync_accounts_receivable_document_job_spec.rb
+++ spec/jobs/itms/sync_accounts_receivable_document_job_spec.rb
@@ -28,26 +28,25 @@ RSpec.describe Itms::SyncAccountsReceivableDocumentJob, type: :job do
         konkretny_ciel: Itms::SpecificGoal.find_by!(itms_id: 8),
         nezrovnalost: Itms::Discrepancy.find_by!(itms_id: 4),
         prioritna_os: Itms::PriorityAxis.find_by!(itms_id: 8),
+        projekt: Itms::Project.find_by!(itms_id: 12),
         stav: 'Vysporiadaný',
         subjekt_zodpovedny_za_vymahanie: Itms::Subject.find_by!(itms_id: 100003),
         suma_na_vratenie: 773.79,
         suma_na_vratenie_zdroj_eu: 657.72,
         suma_na_vratenie_zdroj_pr: nil,
         suma_na_vratenie_zdroj_sr: 116.07,
+        suvisiace_verejne_obstaravania: [
+            Itms::Procurement.find_by!(itms_id: 219)
+        ],
+        suvisiace_zop: [
+            Itms::PaymentClaim.find_by!(itms_id: 123)
+        ],
         typ: 'ZOVFP',
         vracia_sa_do_rozpoctu_projektu: false,
         vratena_suma: 773.79,
         vratena_suma_zdroj_eu: 657.72,
         vratena_suma_zdroj_pr: nil,
         vratena_suma_zdroj_sr: 116.07,
-      )
-    end
-
-    pending 'syncs second-level and 1-to-M attributes' do
-      expect(Itms::AccountsReceivableDocument.first).to respond_to(
-        :projekt,
-        :suvisiace_verejne_obstaravania,
-        :suvisiace_zop
       )
     end
   end
