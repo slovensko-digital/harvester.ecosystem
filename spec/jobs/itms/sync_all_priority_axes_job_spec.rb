3edb787f218e18067809d00e8928d8e2fca583c0
Ernest Walzel <ewalzel@gmail.com>
2017-11-22 10:25:37 +0100

    Itms::SyncAllPriorityAxesJob

---
+++ spec/jobs/itms/sync_all_priority_axes_job_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Itms::SyncAllPriorityAxesJob, type: :job do
+  let(:downloader) { double(:downloader) }
+
+  context '#perform' do
+    ActiveJob::Base.queue_adapter = :test
+
+    it 'syncs all specific goals' do
+      expect(downloader)
+          .to receive(:get)
+          .with('https://opendata.itms2014.sk/v2/operacneProgramy')
+          .and_return(double(body: itms_file_fixture('operacny_program_list.json')))
+          .once
+
+      expect(downloader)
+          .to receive(:get)
+          .with(/\/v2\/operacneProgramy\/\d+\/prioritneOsi$/)
+          .and_return(double(body: itms_file_fixture('prioritna_os_list.json')))
+          .exactly(10).times
+
+      subject.perform(downloader: downloader)
+
+      expect(Itms::SyncPriorityAxisJob).to have_been_enqueued.exactly(10 * 5).times
+      expect(Itms::SyncPriorityAxisJob).to have_been_enqueued.with('/v2/prioritnaOs/8').at_least(:once)
+      expect(Itms::SyncPriorityAxisJob).to have_been_enqueued.with('/v2/prioritnaOs/9').at_least(:once)
+      expect(Itms::SyncPriorityAxisJob).to have_been_enqueued.with('/v2/prioritnaOs/10').at_least(:once)
+    end
+  end
+end
