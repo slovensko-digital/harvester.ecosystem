2cad8402dcc30b61957b118d9e9b8db86df53a3c
Ernest Walzel <ewalzel@gmail.com>
2017-11-22 10:57:01 +0100

    Itms::SyncAllSubjectsJob, Itms::SyncAllSuppliersJob

---
+++ spec/jobs/itms/sync_all_subjects_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Itms::SyncAllSubjectsJob, type: :job do
+  let(:downloader) { double(:downloader) }
+
+  context '#perform' do
+    ActiveJob::Base.queue_adapter = :test
+
+    it 'syncs all subjects' do
+      Itms::Subject.create(itms_id: 1, itms_href: '/v2/subjekty/1')
+      Itms::Subject.create(itms_id: 2, itms_href: '/v2/subjekty/2')
+      Itms::Subject.create(itms_id: 3, itms_href: '/v2/subjekty/3')
+
+      subject.perform
+
+      expect(Itms::SyncSubjectJob).to have_been_enqueued.exactly(3).times
+      expect(Itms::SyncSubjectJob).to have_been_enqueued.with('/v2/subjekty/1')
+      expect(Itms::SyncSubjectJob).to have_been_enqueued.with('/v2/subjekty/2')
+      expect(Itms::SyncSubjectJob).to have_been_enqueued.with('/v2/subjekty/3')
+    end
+  end
+end
