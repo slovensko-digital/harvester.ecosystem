fe38a73ebf30d7dcb1db3f013def2d8dff129916
Ernest Walzel <ewalzel@gmail.com>
2017-11-22 15:04:33 +0100

    Clean up relationships for Itms::AnnouncedProposalCall Remove redundant models.

--- app/models/itms/announced_proposal_call_contact_person.rb
+++
@@ -1,6 +0,0 @@
-class Itms::AnnouncedProposalCallContactPerson < ApplicationRecord
-  self.table_name = 'itms.vyzvy_vyhlasene_kontaktne_osoby'
-
-  belongs_to :vyzva, class_name: Itms::AnnouncedProposalCall
-  belongs_to :osoba, class_name: Itms::Person
-end
