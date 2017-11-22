7134bca37236bdd3940cb0d5f22dd959a75a3db0
Ernest Walzel <ewalzel@gmail.com>
2017-11-22 12:08:51 +0100

    Change HMT relationships to HABTM for Itms::Procurement Remove redundant join models

--- app/models/itms/procurement_operational_program.rb
+++
@@ -1,6 +0,0 @@
-class Itms::ProcurementOperationalProgram < ApplicationRecord
-  self.table_name = 'itms.verejne_obstaravania_operacne_programy'
-
-  belongs_to :verejne_obstaravanie, class_name: Itms::Procurement
-  belongs_to :operacny_program, class_name: Itms::OperationalProgram
-end
--- app/models/itms/procurement_project.rb
+++
@@ -1,6 +0,0 @@
-class Itms::ProcurementProject < ApplicationRecord
-  self.table_name = 'itms.verejne_obstaravania_projekty'
-
-  belongs_to :verejne_obstaravanie, class_name: Itms::Procurement
-  belongs_to :projekt, class_name: Itms::Project
-end
