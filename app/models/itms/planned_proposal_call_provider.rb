cad320efebc2d396d928b56c75831c7d6a989a09
Ernest Walzel <ewalzel@gmail.com>
2017-11-22 14:48:46 +0100

    Clean up relationships for Itms::PlannedProposalCall Remove redundant models.

--- app/models/itms/planned_proposal_call_provider.rb
+++
@@ -1,6 +0,0 @@
-class Itms::PlannedProposalCallProvider < ApplicationRecord
-  self.table_name = 'itms.vyzvy_planovane_poskytovatelia'
-
-  belongs_to :vyzva, class_name: Itms::PlannedProposalCall
-  belongs_to :poskytovatel, class_name: Itms::Subject
-end
--- app/models/itms/planned_proposal_call_specific_goal.rb
+++
@@ -1,6 +0,0 @@
-class Itms::PlannedProposalCallSpecificGoal < ApplicationRecord
-  self.table_name = 'itms.vyzvy_planovane_konkretne_ciele'
-
-  belongs_to :vyzva, class_name: Itms::PlannedProposalCall
-  belongs_to :konkretny_ciel, class_name: Itms::SpecificGoal
-end
