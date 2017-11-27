96e7b81bf8fd5760cbc66899591ad430e5477736
Ernest Walzel <ewalzel@gmail.com>
2017-11-27 19:19:48 +0100

    Fix block variable names

--- app/jobs/itms/sync_activity_job.rb
+++ app/jobs/itms/sync_activity_job.rb
@@ -3,19 +3,19 @@ class Itms::SyncActivityJob < ItmsJob
     json = downloader.get_json_from_href(itms_href)
 
     ActiveRecord::Base.transaction do
-      pa = Itms::Activity.find_or_create_by!(itms_id: json['id'])
-      pa.itms_href = json['href']
+      a = Itms::Activity.find_or_create_by!(itms_id: json['id'])
+      a.itms_href = json['href']
 
-      pa.datum_konca_planovany = json['datumKoncaPlanovany']
-      pa.datum_konca_skutocny = json['datumKoncaSkutocny']
-      pa.datum_zaciatku_planovany = json['datumZaciatkuPlanovany']
-      pa.datum_zaciatku_skutocny = json['datumZaciatkuSkutocny']
-      pa.kod = json['kod']
-      pa.nazov = json['nazov']
-      pa.subjekt = find_or_create_subject_by_json(json['subjekt'], downloader)
-      pa.typ_aktivity = find_or_create_activity_type_by_json(json['typAktivity'], downloader)
+      a.datum_konca_planovany = json['datumKoncaPlanovany']
+      a.datum_konca_skutocny = json['datumKoncaSkutocny']
+      a.datum_zaciatku_planovany = json['datumZaciatkuPlanovany']
+      a.datum_zaciatku_skutocny = json['datumZaciatkuSkutocny']
+      a.kod = json['kod']
+      a.nazov = json['nazov']
+      a.subjekt = find_or_create_subject_by_json(json['subjekt'], downloader)
+      a.typ_aktivity = find_or_create_activity_type_by_json(json['typAktivity'], downloader)
 
-      pa.save!
+      a.save!
     end
   end
 end
