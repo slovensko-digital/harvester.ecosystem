12c915724a1ce28b38e05690eeec4aa97bca1bf4
Ernest Walzel <ewalzel@gmail.com>
2017-12-22 10:53:51 +0100

    Backfill timestamps for itms.zmluvy_verejne_obstaravania

--- db/migrate/20171221222028_itms_add_timestamps_to_zmluvy_verejne_obstaravania.rb
+++ db/migrate/20171221222028_itms_add_timestamps_to_zmluvy_verejne_obstaravania.rb
@@ -1,5 +1,8 @@
 class ItmsAddTimestampsToZmluvyVerejneObstaravania < ActiveRecord::Migration[5.0]
   def change
-    add_timestamps 'itms.zmluvy_verejne_obstaravania'
+    add_timestamps 'itms.zmluvy_verejne_obstaravania', null: true
+    execute "UPDATE itms.zmluvy_verejne_obstaravania SET created_at=now(), updated_at=now()"
+    change_column 'itms.zmluvy_verejne_obstaravania', :created_at, :timestamp, null: false
+    change_column 'itms.zmluvy_verejne_obstaravania', :updated_at, :timestamp, null: false
   end
 end
