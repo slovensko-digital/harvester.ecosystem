4426027ba19a12ef31981588c43b1b99c37e7996
Pavol Zbell <pavol.zbell@gmail.com>
2018-09-17 18:16:38 +0200

    Reduce method complexity

--- app/jobs/upvs/fetch_public_authority_edesks_list_job.rb
+++ app/jobs/upvs/fetch_public_authority_edesks_list_job.rb
@@ -11,23 +11,8 @@ class Upvs::FetchPublicAuthorityEdesksListJob < ApplicationJob
     TemporaryPublicAuthorityEdesk.transaction do
       TemporaryPublicAuthorityEdesk.create_table!
 
-      CSV.foreach(csv_file, csv_options) do |row|
-        row = row.to_h.transform_keys { |k| k.to_s.gsub(/\p{Cf}/, '') }
-
-        uri = row.fetch('URI')
-        cin = row['IČO'] || row['IČO'] || row.fetch('ICO')
-
-        raise "#{uri} does not match #{cin}" if uri !~ /ico:\/\/sk\/(0*)#{cin}(_\d+)?/
-
-        TemporaryPublicAuthorityEdesk.create!(
-          uri: uri,
-          cin: cin,
-          name: row['NAZOV INŠTITÚCIE'] || row.fetch('NÁZOV'),
-          street: row.fetch('ULICA'),
-          street_number: row.fetch('ČÍSLO'),
-          postal_code: row.fetch('PSČ'),
-          city: row.fetch('MESTO'),
-        )
+      each_row_as_attributes(csv_file, csv_options) do |attributes|
+        TemporaryPublicAuthorityEdesk.create!(attributes)
       end
 
       TemporaryPublicAuthorityEdesk.truncate_source_table!
@@ -38,6 +23,29 @@ class Upvs::FetchPublicAuthorityEdesksListJob < ApplicationJob
   class TemporaryPublicAuthorityEdesk < TemporaryRecord
     def self.source
       Upvs::PublicAuthorityEdesk
+    end
+  end
+
+  private
+
+  def each_row_as_attributes(csv_file, csv_options)
+    CSV.foreach(csv_file, csv_options) do |row|
+      row = row.to_h.transform_keys { |k| k.to_s.gsub(/\p{Cf}/, '') }
+
+      uri = row.fetch('URI')
+      cin = row['IČO'] || row['IČO'] || row.fetch('ICO')
+
+      raise "#{uri} does not match #{cin}" if uri !~ /ico:\/\/sk\/(0*)#{cin}(_\d+)?/
+
+      yield(
+        uri: uri,
+        cin: cin,
+        name: row['NAZOV INŠTITÚCIE'] || row.fetch('NÁZOV'),
+        street: row.fetch('ULICA'),
+        street_number: row.fetch('ČÍSLO'),
+        postal_code: row.fetch('PSČ'),
+        city: row.fetch('MESTO')
+      )
     end
   end
 end
