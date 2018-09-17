672ac75dfbcf92a0d6e9141c0915411ea705a32f
Pavol Zbell <pavol.zbell@gmail.com>
2018-09-17 18:06:09 +0200

    Import eDesks via temporary table with additional checks

--- app/jobs/upvs/fetch_public_authority_edesks_list_job.rb
+++ app/jobs/upvs/fetch_public_authority_edesks_list_job.rb
@@ -2,33 +2,42 @@ require 'csv'
 require 'harvester_utils/downloader'
 
 class Upvs::FetchPublicAuthorityEdesksListJob < ApplicationJob
-  # TODO timeouts
   queue_as :upvs
 
   def perform(url, downloader: HarvesterUtils::Downloader)
-    file = downloader.download_file(url)
-    import_csv(file)
-  end
+    csv_file = downloader.download_file(url)
+    csv_options = { col_sep: File.open(csv_file) { |f| f.readline }.include?(';') ? ';' : ',', headers: true }
 
-  private
+    TemporaryPublicAuthorityEdesk.transaction do
+      TemporaryPublicAuthorityEdesk.create_table!
 
-  def import_csv(file)
-    header = File.open(file) { |f| f.readline }
-    separator = detect_separator(header)
-    CSV.foreach(file, headers: true, col_sep: separator) do |row|
-      row = row.to_h.transform_keys { |k| k.to_s.gsub(/\p{Cf}/, '') }
-      edesk = Upvs::PublicAuthorityEdesk.find_or_initialize_by(uri: row.fetch('URI'))
-      edesk.cin = row['IČO'] || row['IČO'] || row.fetch('ICO')
-      edesk.name = row['NAZOV INŠTITÚCIE'] || row.fetch('NÁZOV')
-      edesk.street = row.fetch('ULICA')
-      edesk.street_number = row.fetch('ČÍSLO')
-      edesk.postal_code = row.fetch('PSČ')
-      edesk.city = row.fetch('MESTO')
-      edesk.save!
+      CSV.foreach(csv_file, csv_options) do |row|
+        row = row.to_h.transform_keys { |k| k.to_s.gsub(/\p{Cf}/, '') }
+
+        uri = row.fetch('URI')
+        cin = row['IČO'] || row['IČO'] || row.fetch('ICO')
+
+        raise "#{uri} does not match #{cin}" if uri !~ /ico:\/\/sk\/(0*)#{cin}(_\d+)?/
+
+        TemporaryPublicAuthorityEdesk.create!(
+          uri: uri,
+          cin: cin,
+          name: row['NAZOV INŠTITÚCIE'] || row.fetch('NÁZOV'),
+          street: row.fetch('ULICA'),
+          street_number: row.fetch('ČÍSLO'),
+          postal_code: row.fetch('PSČ'),
+          city: row.fetch('MESTO'),
+        )
+      end
+
+      TemporaryPublicAuthorityEdesk.truncate_source_table!
+      TemporaryPublicAuthorityEdesk.insert_to_source_table!
     end
   end
 
-  def detect_separator(header)
-    header.include?(';') ? ';' : ','
+  class TemporaryPublicAuthorityEdesk < TemporaryRecord
+    def self.source
+      Upvs::PublicAuthorityEdesk
+    end
   end
 end
--- spec/jobs/upvs/fetch_public_authority_edesks_list_job_spec.rb
+++ spec/jobs/upvs/fetch_public_authority_edesks_list_job_spec.rb
@@ -95,5 +95,45 @@ RSpec.describe Upvs::FetchPublicAuthorityEdesksListJob, type: :job do
 
       expect(Upvs::PublicAuthorityEdesk.count).to eq(5)
     end
+
+    context 'eDesks list already imported' do
+      it 'clears eDesks list, then imports eDesks' do
+        create_list(:upvs_public_authority_edesk, 10)
+
+        expect(Upvs::PublicAuthorityEdesk.count).to eq(10)
+        expect(downloader).to receive(:download_file).with(url).and_return(fixture_filepath('upvs/edesks-v1.csv'))
+
+        subject.perform(url, downloader: downloader)
+
+        expect(Upvs::PublicAuthorityEdesk.count).to eq(5)
+      end
+    end
+
+    context 'eDesks list does not match URI with CIN' do
+      it 'does not import public authority eDesks' do
+        expect(downloader).to receive(:download_file).with(url).and_return(fixture_filepath('upvs/edesks-v5-not-matching.csv'))
+
+        expect { subject.perform(url, downloader: downloader) }.to raise_error(RuntimeError)
+
+        expect(Upvs::PublicAuthorityEdesk.count).to eq(0)
+      end
+
+      it 'retains previously imported public authority eDesks' do
+        create_list(:upvs_public_authority_edesk, 10)
+
+        expect(Upvs::PublicAuthorityEdesk.count).to eq(10)
+        expect(downloader).to receive(:download_file).with(url).and_return(fixture_filepath('upvs/edesks-v5-not-matching.csv'))
+
+        expect { subject.perform(url, downloader: downloader) }.to raise_error(RuntimeError)
+
+        expect(Upvs::PublicAuthorityEdesk.count).to eq(10)
+      end
+
+      it 'raises custom error' do
+        expect(downloader).to receive(:download_file).with(url).and_return(fixture_filepath('upvs/edesks-v5-not-matching.csv'))
+
+        expect { subject.perform(url, downloader: downloader) }.to raise_error('ico://sk/92156427_90000 does not match 42156424')
+      end
+    end
   end
 end
