e2caad209e091864aaaa4480ce91221d5e6d985a
Pavol Zbell <pavol.zbell@gmail.com>
2019-11-07 13:31:17 +0100

    Schedule UPVS find services with forms task

--- clock.rb
+++ clock.rb
@@ -9,6 +9,7 @@ module Clockwork
   end
 
   every(1.week, 'upvs:public_authority_edesks:sync', at: 'Sunday 4:00')
+  every(1.week, 'upvs:services_with_forms:sync', at: 'Sunday 4:00')
 
   every(1.day, 'itms:sync', at: '5:00')
   every(1.day, 'itms:detect_api_changes', at: '9:00')
