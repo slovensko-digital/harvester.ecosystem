8dba26cb3d74a7f00095a46007719ebb8bda32a6
michalrohacek256 <michal.rohacek.256@gmail.com>
2020-04-24 17:14:05 +0200

    Change job to skip supplier if 404

--- app/jobs/itms/sync_supplier_job.rb
+++ app/jobs/itms/sync_supplier_job.rb
@@ -1,5 +1,10 @@
 class Itms::SyncSupplierJob < ItmsJob
   def perform(itms_href, downloader: ItmsJob::Downloader)
+    unless downloader.href_exists?(itms_href)
+      Itms::Supplier.find_by(itms_id: parse_id(itms_href))&.touch(:deleted_at)
+      return
+    end
+
     json = downloader.get_json_from_href(itms_href)
 
     ActiveRecord::Base.transaction do
@@ -20,5 +25,9 @@ class Itms::SyncSupplierJob < ItmsJob
       s.ulica_cislo = json['ulicaCislo']
       s.save!
     end
+  end
+
+  def parse_id(href)
+    Integer(href.split('/').last)
   end
 end
