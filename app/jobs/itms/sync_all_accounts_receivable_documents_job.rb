fb91eb88d69b93bc0aa9ccb02cf78295d00a2670
michalrohacek256 <michal.rohacek.256@gmail.com>
2020-05-01 02:04:02 +0200

    Apply change on jobs

--- app/jobs/itms/sync_all_accounts_receivable_documents_job.rb
+++ app/jobs/itms/sync_all_accounts_receivable_documents_job.rb
@@ -1,6 +1,6 @@
 class Itms::SyncAllAccountsReceivableDocumentsJob < ItmsJob
   def perform(downloader: ItmsJob::Downloader)
-    json = downloader.get_json_from_href('/v2/pohladavkovyDoklad')
+    json = downloader.get_json_from_href('/v2/pohladavkovyDoklad', modifiedSince: latest_timestamp)
     hrefs = json.map { |item| item['href'] }
     hrefs.each { |href| Itms::SyncAccountsReceivableDocumentJob.perform_later(href) }
   end
--- app/jobs/itms/sync_all_activities_job.rb
+++ app/jobs/itms/sync_all_activities_job.rb
@@ -1,6 +1,6 @@
 class Itms::SyncAllActivitiesJob < ItmsJob
   def perform(downloader: ItmsJob::Downloader)
-    json = downloader.get_json_from_href('/v2/aktivita')
+    json = downloader.get_json_from_href('/v2/aktivita', modifiedSince: latest_timestamp)
     hrefs = json.map { |item| item['href'] }
     hrefs.each { |href| Itms::SyncActivityJob.perform_later(href) }
   end
