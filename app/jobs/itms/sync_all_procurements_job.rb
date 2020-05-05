1a523941ae6835424b42acaa5c792c4e94f745df
michalrohacek256 <michal.rohacek.256@gmail.com>
2020-05-05 20:07:16 +0200

    Refactor another bunch of jobs

--- app/jobs/itms/sync_all_procurements_job.rb
+++ app/jobs/itms/sync_all_procurements_job.rb
@@ -1,7 +1,6 @@
 class Itms::SyncAllProcurementsJob < ItmsJob
   def perform(downloader: ItmsJob::Downloader)
-    json = downloader.get_json_from_href('/v2/verejneObstaravania')
-    hrefs = json.map { |item| item['href'] }
-    hrefs.each { |href| Itms::SyncProcurementJob.perform_later(href) }
+    json = downloader.get_json_from_href('/v2/verejneObstaravania', modifiedSince: latest_procurement_timestamp)
+    json.each { |item| Itms::SyncProcurementJob.perform_later(item['href']) }
   end
 end
