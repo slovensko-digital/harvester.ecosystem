2247bb122358ca553776e9280c1e90706c993914
Stanislav Modrak <stanislav.modrak@gmail.com>
2020-07-10 22:30:40 +0200

    Bump Rails to 5.1.x

--- config/initializers/new_framework_defaults_5_1.rb
+++ config/initializers/new_framework_defaults_5_1.rb
@@ -1,10 +1,21 @@
 # Be sure to restart your server when you modify this file.
 #
-# This file contains migration options to ease your Rails 5.0 upgrade.
+# This file contains migration options to ease your Rails 5.1 upgrade.
 #
 # Once upgraded flip defaults one by one to migrate to the new default.
 #
 # Read the Guide for Upgrading Ruby on Rails for more info on each option.
+
+# new_framework_defaults_5_1 below
+
+# Make `form_with` generate non-remote forms.
+Rails.application.config.action_view.form_with_generates_remote_forms = false
+
+# Unknown asset fallback will return the path passed in when the given
+# asset is not present in the asset pipeline.
+# Rails.application.config.assets.unknown_asset_fallback = false
+
+# new_framework_defaults_5_0 below
 
 # Enable per-form CSRF tokens. Previous versions had false.
 Rails.application.config.action_controller.per_form_csrf_tokens = false
