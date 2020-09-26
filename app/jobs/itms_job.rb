4c71187f127fab2778eddf83f9909ab7b0fdd454
michal-rohacek <michal.rohacek.256@gmail.com>
2020-09-26 16:21:04 +0200

    Change Typhoeus to Faraday

--- app/jobs/itms_job.rb
+++ app/jobs/itms_job.rb
@@ -28,8 +28,8 @@ class ItmsJob < ApplicationJob
 
     def self.get(url)
       response = downloader.get(url)
-      raise NotFoundError, "Url not found: #{url}" if response.code == 404
-      raise DownloadError, "Unexpected response code: #{response.code} for url: #{url}" if response.code != 200
+      raise NotFoundError, "Url not found: #{url}" if response.status == 404
+      raise DownloadError, "Unexpected response status: #{response.status} for url: #{url}" if response.status != 200
       response
     end
   end
