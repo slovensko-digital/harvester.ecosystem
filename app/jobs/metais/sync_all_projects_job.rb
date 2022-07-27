fde4d5713f8c924700708a726ce354fa0bead7d4
Marek Ceľuch <celuchmarek@gmail.com>
2022-07-27 14:28:38 +0200

    download metais projects in smaller batches

--- app/jobs/metais/sync_all_projects_job.rb
+++ app/jobs/metais/sync_all_projects_job.rb
@@ -8,17 +8,17 @@ class Metais::SyncAllProjectsJob < ApplicationJob
   def perform()
     conn = Faraday.new(url: API_ENDPOINT)
     page_number = 1
-    projects = []
 
     loop do
+      projects = []
       response = conn.post('', '{"filter":{"type":["Projekt"],"metaAttributes":{"state":["DRAFT"]}},"page":%{page},"perpage":5}' % {page: page_number}, 'Content-Type' => 'application/json')
-      projects.concat(JSON.parse(response.body)&.dig('configurationItemSet'))
+      projects = JSON.parse(response.body)&.dig('configurationItemSet')
 
       page_number >= JSON.parse(response.body)['pagination']['totalPages'] ? break : page_number += 1
-    end
 
-    projects.each do |project|
-      Metais::SyncProjectJob.perform_later(project)
+      projects.each do |project|
+        Metais::SyncProjectJob.perform_later(project)
+      end
     end
   end
 end
