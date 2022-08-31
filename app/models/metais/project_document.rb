a5830028f4f84fc9bf945124d0a6cd10739736cd
Marek Ceľuch <celuchmarek@gmail.com>
2022-08-31 18:51:30 +0200

    fix metais optional latest version

--- app/models/metais/project_document.rb
+++ app/models/metais/project_document.rb
@@ -3,6 +3,6 @@ class Metais::ProjectDocument < ApplicationRecord
 
   belongs_to :project, class_name: 'Metais::Project', foreign_key: 'project_id'
   has_many :versions, class_name: 'Metais::ProjectDocumentVersion', foreign_key: 'document_id'
-  belongs_to :latest_version, class_name: 'Metais::ProjectDocumentVersion', foreign_key: 'latest_version_id'
+  belongs_to :latest_version, class_name: 'Metais::ProjectDocumentVersion', foreign_key: 'latest_version_id', optional: true
 end
   
