12959f6d1b3459d6bac0013177f02bd75563cc98
Marek Ceľuch <celuchmarek@gmail.com>
2022-09-19 14:31:27 +0200

    change scope for previous version

--- app/jobs/metais/diff_consecutive_versions_job.rb
+++ app/jobs/metais/diff_consecutive_versions_job.rb
@@ -4,7 +4,9 @@ class Metais::DiffConsecutiveVersionsJob < ApplicationJob
   OBSERVED_ATTRIBUTES = ['nazov', 'kod_metais', 'typ_investicie', 'prijimatel', 'faza_projektu', 'program', 'popis', 'datum_zacatia', 'termin_ukoncenia', 'schvalovaci_proces', 'zdroj', 'financna_skupina', 'suma_vydavkov', 'rocne_naklady', 'ref_id', 'status', 'zmena_stavu', 'schvalene_rocne_naklady', 'schvaleny_rozpocet', 'datum_nfp', 'link_nfp', 'vyhlasenie_vo', 'vo', 'zmluva_o_dielo', 'zmluva_o_dielo_crz']
 
   def perform(version)
-    previous = Metais::ProjectVersion.previous(version)
+    result = Metais::ProjectVersion.previous(version)
+    return if result.blank?
+    previous = result.first
 
     OBSERVED_ATTRIBUTES.each do |key|
       Metais::ProjectChange.create(
--- app/models/metais/project_version.rb
+++ app/models/metais/project_version.rb
@@ -3,5 +3,9 @@ class Metais::ProjectVersion < ApplicationRecord
 
   belongs_to :project, class_name: 'Metais::Project', foreign_key: 'project_id'
 
-  scope :previous, -> (version) { version.project.versions.where("created_at < ?", version.created_at).order(created_at: :desc).first }
+  scope :previous, -> (version) { 
+    where("project_id = ? AND created_at < ?", version.project.id, version.created_at)
+    .order(created_at: :desc)
+    .limit(1)
+  }
 end
