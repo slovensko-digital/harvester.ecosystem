8c55846fdfa33deb05149f4cadb803dd81c113b5
Tomáš Drga <tomasxdrga@gmail.com>
2023-08-25 14:40:17 +0200

    Changes made

--- app/jobs/upvs/fetch_services_with_forms_list_job.rb
+++ app/jobs/upvs/fetch_services_with_forms_list_job.rb
@@ -36,11 +36,11 @@ class Upvs::FetchServicesWithFormsListJob < ApplicationJob
   def each_row_as_attributes(csv_file, csv_options)
 
     CSV.foreach(csv_file, csv_options) do |row|
-      row = row.to_h.transform_keys { |k| k.to_s.gsub(/\p{Cf}|"/, '') }
-
+      row = row.to_h.transform_keys { |k| k.to_s.gsub!(/\p{Cf}|"/, '') }
       row = row.to_h.transform_keys { |k| k.to_s }
-      row = row.transform_values { |v| v == 'NULL' ? nil : v }
+
       row = row.transform_values { |value| value.gsub!(/[\\"]/,'') }
+      row = row.transform_values { |v| v == 'NULL' ? nil : v }
 
       row["IdServiceInstance"], row["ExternalCode"] = row["IdServiceInstance,ExternalCode"].split(',')
       row.delete("IdServiceInstance,ExternalCode")
