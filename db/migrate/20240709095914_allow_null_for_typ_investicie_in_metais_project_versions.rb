af5935f161cfdf1ae27557d957dad30903ae4006
Tomáš Drga <tomasxdrga@gmail.com>
2024-07-09 12:43:25 +0200

    Migration with correct pg_dump

--- db/migrate/20240709095914_allow_null_for_typ_investicie_in_metais_project_versions.rb
+++ db/migrate/20240709095914_allow_null_for_typ_investicie_in_metais_project_versions.rb
@@ -1,5 +1,5 @@
 class AllowNullForTypInvesticieInMetaisProjectVersions < ActiveRecord::Migration[6.0]
   def change
-    change_column_null 'metais.project_versions', :typ_investicie, null: true
+    change_column_null 'metais.project_versions', :typ_investicie, true
   end
 end
